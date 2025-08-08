b717975f783366923d96a66c9aeac1d92d67a862
Vipul Mane <113690721+vipulmane6498@users.noreply.github.com>
2025-08-08 14:34:37 +0530

    Add files via upload

---
+++ 01_Star Patterns/26_HourGlassStarPattern.cs
@@ -0,0 +1,89 @@
+
+/*
+Hour Glass Star Pattern:
+
+Approach:
+1. Get the number of rows from the user.
+2. Calculate the middle point of the hourglass as n / 2.
+3. First half of the hourglass:
+   - Outer loop iterates from 0 to less than mid for rows.
+   - 1st Inner loop prints spaces before the stars.
+   - 2nd inner loop prints stars followed by a space.
+   - Print a new line after each row to form the first half of the hourglass.
+4. Second half of the hourglass:
+   - Outer loop iterates from mid-2 to 0 for rows.  
+    - 1st Inner loop prints spaces before the stars.
+    - 2nd inner loop prints stars followed by a space.
+    - Print a new line after each row to form the second half of the hourglass.
+
+E.g.: 
+Enter number of rows: 10
+* * * * * 
+ * * * * 
+  * * * 
+   * * 
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * * 
+
+*/
+
+using System;
+
+public class HourGlassStarPattern
+{
+    public static void Main(string[] args)
+    {
+
+     Console.Write("Enter number of rows: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int mid = n / 2;
+
+        for (int i = 0; i < mid; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                System.Console.Write(" ");
+            }
+            for (int k = 0; k < mid - i; k++)
+            {
+                System.Console.Write("* ");
+            }
+            System.Console.WriteLine();
+        }
+
+        for (int i = mid - 2; i >= 0; i--)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                System.Console.Write(" ");
+            }
+            for (int k = 0; k < mid - i; k++)
+            {
+                System.Console.Write("* ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+/*
+
+OTUUT:
+
+Enter number of rows:
+
+* * * * * 
+ * * * * 
+  * * * 
+   * * 
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * *
+ 
+*/
